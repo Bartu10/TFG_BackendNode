d281edb279511e4094b0ac2c0ed78bc54401aecf
Bartu10 <jbg10102002@gmail.com>
2024-02-20 17:24:01 +0100

    Fin?

--- factories/ProductFactory.js
+++ factories/ProductFactory.js
@@ -6,26 +6,27 @@ class ProductFactory {
 
   }
 
-    generate() {
-      const year = String(faker.datatype.number({ min: 1970, max: 2022 }));
-      const retro = parseInt(year) < 2018;
-  
-      return {
-        id: this.id++,
-        name: `Camiseta ${this.getRandomTeam()} ${year}`,
-        description: faker.lorem.paragraph(2),
-        price: faker.datatype.number({ min: 40, max: 100 }),
-        state: faker.random.arrayElement(["Nuevo", "Usado"]),
-        yr: year,
-        retro: retro,
-        team: this.getRandomTeam(),
-        imageUrl: "https://i.imgur.com/xvgMf2U.jpg",
-        XL: faker.datatype.number({ min: 0, max: 200 }),
-        L: faker.datatype.number({ min: 0, max: 200 }),
-        M: faker.datatype.number({ min: 0, max: 200 }),
-        S: faker.datatype.number({ min: 0, max: 200 })
-      };
-    }
+  generate() {
+    const year = String(faker.datatype.number({ min: 1970, max: 2022 }));
+    const retro = parseInt(year) < 2018;
+    const team = this.getRandomTeam();
+
+    return {
+      id: this.id++,
+      name: `Camiseta ${team} ${year}`,
+      description: faker.lorem.paragraph(2),
+      price: faker.datatype.number({ min: 40, max: 100 }),
+      state: faker.random.arrayElement(["Nuevo", "Usado"]),
+      yr: year,
+      retro: retro,
+      team: team,
+      imageUrl: "https://i.imgur.com/xvgMf2U.jpg",
+      XL: faker.datatype.number({ min: 0, max: 200 }),
+      L: faker.datatype.number({ min: 0, max: 200 }),
+      M: faker.datatype.number({ min: 0, max: 200 }),
+      S: faker.datatype.number({ min: 0, max: 200 })
+    };
+  }
   
     getRandomTeam() {
       const teams = [
